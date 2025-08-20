180531385f8aff594fede17907a4679c41b018a4
crisveg24 <ccvegas@academia.usbbog.edu.co>
2025-08-20 16:28:31 +0000

    creamos un lector del data frame

--- Extract/store_branchesExtractor.py
+++ Extract/store_branchesExtractor.py
@@ -5,8 +5,7 @@ import numpy as np
 class store_branches:
     def __init__(self, csv_path):
         self.csv = csv_path
-def queries(self)
-    data = pd.read_csv(self.csv)
-def response (
-        return data.head(5)
-)
+    def queries(self):
+        data = pd.read_csv(self.csv)
+    def response (self):
+        return self.data.head(5)
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 from store_branchesExtract import store_branchesExtractor
+response1 = store_branchesExtractor("csv")
+response1.queries ()
+print (response1.response)
